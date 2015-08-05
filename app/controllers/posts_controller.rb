159bd41965aed4e04123cf8fcea5e8e11e4fd265
John Master <john.s.master@gmail.com>
2015-08-05 12:57:21 -0400

    add post routes and add home to navigation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,35 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find(params[:id])
   end
+
+  def create
+    @manager = Manager.find(session[:manager]['id'])
+    @post = @manager.posts.new(post_params)
+    if @post.save
+      redirect_to @manager
+    else
+      render @manager
+    end
+   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @manager = Manager.find(session[:manager]["id"]) #I really don't like this. Is there something better?
+    @post = Post.find(params[:id])
+    @post.destroy #should add some warning
+    redirect_to @manager
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :manager
   has_many :bids, dependent: :destroy
+  has_many :users, through: :bids
 end
